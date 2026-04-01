8eefab84f90089970711ab5068a2dd73ea82e247
hrqric <cauehrqric@hotmail.com>
2026-03-31 21:33:51 -0300

    feat: add Dockerfile and configure CORS policy in Program.cs

--- VOID.VSS/Program.cs
+++ VOID.VSS/Program.cs
@@ -14,6 +14,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+
 var app = builder.Build();
 if(app.Environment.IsDevelopment())
 {
@@ -21,6 +30,7 @@ if(app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("/", () => "Starting Void Stock System API... ");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
